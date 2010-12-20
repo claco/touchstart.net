1412a46112216f34426df77ec90d7710af11de4e
Christopher H. Laco <claco@chrislaco.com>
2010-12-19 21:39:23 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TouchstartNet::Application.config.secret_token = '3620166ba550cd1b43256ada70ee4ca8613999474ce1e95fa93e889c3a5fc2b0e006352b83f4f727c2fa786cb5243301b70de473ac637f39b9e58afdf4cfd897'
